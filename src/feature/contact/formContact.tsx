beeddd880865fe3fe8477484f22bf5f0373a7935
CristianDev <camilovargas_12@misena.edu.co>
2025-05-28 17:12:12 -0500

    CCVO: PAGUE CONTACTO

--- src/feature/contact/formContact.tsx
+++ src/feature/contact/formContact.tsx
@@ -1,9 +1,49 @@
-import React from "react";
+import { Button, Input } from "../core/ui";
+import { Textarea } from "../core/ui/textarea";
+import { useForm } from "react-hook-form";
+import {
+  createDocumentSchema,
+  type CreateDocumentSchemaType,
+} from "./createDocumetShema";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 export const FormContact = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CreateDocumentSchemaType>({
+    resolver: zodResolver(createDocumentSchema),
+  });
+
+  const onSubmit = (data: CreateDocumentSchemaType) => {
+    console.log(data);
+  };
+
   return (
-    <div>
-      <form></form>
+    <div className="w-10/12 mx-auto  justify-center items-center mt-5">
+      <form
+        onSubmit={handleSubmit(onSubmit, (errors) => console.log(errors))}
+        className="grid grid-cols-3 gap-5 "
+      >
+        <div className="col-span-1">
+          <Input placeholder="Nombre Completo" {...register("name")} />
+        </div>
+        <div className="col-span-1">
+          <Input placeholder="Correo  electrónico" {...register("email")} />
+        </div>
+        <div className="col-span-1">
+          <Input placeholder="Teléfono" {...register("phone")} />
+        </div>
+
+        <div className="col-span-3">
+          <Textarea placeholder="Mensaje" {...register("message")} />
+        </div>
+
+        <div>
+          <Button type="submit">Enviar</Button>
+        </div>
+      </form>
     </div>
   );
 };
--- src/feature/contact/index.ts
+++ src/feature/contact/index.ts
@@ -1,1 +1,2 @@
 export * from "./formContact";
+export * from "./createDocumetShema";
